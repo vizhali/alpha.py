d913a4d8f94bb5db2331bcd4a2d6e8cee0907893
vizhali <52244020+vizhali@users.noreply.github.com>
2019-07-01 10:40:11 +0530

    Create alpha1.py

---
+++ alpha1.py
@@ -0,0 +1,5 @@
+k=input("")
+if k=='a'or k=='b'or k=='c'or k=='d'or k=='e'or k=='f'or k=='g'or k=='h'or k=='i'or k=='j'or k=='k'or k=='l'or k=='m'or k=='n'or k=='o'or k=='p'or k=='q'or k=='r'or k=='s'or k=='t'or k=='u'or k=='v'or k=='w'or k=='x'or k=='y'or k=='z'or k=='A'or k=='B'or k=='C'or k=='D'or k=='E'or k=='F'or k=='G'or k=='H'or k=='I'or k=='J'or k=='K'or k=='L'or k=='M'or k=='N'or k=='O'or k=='P'or k=='R'or k=='S'or k=='t'or k=='U'or k=='V'or  k=='W'or k=='X'or k=='Y'or k=='Z':
+    print("Alphabet")
+else:
+    print("No")
